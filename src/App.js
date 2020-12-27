d1b44a13f15e57f68fe17d88f0fe8d55acd62b18
Kęstutis Grumodas <grumodas.kestutis@gmail.com>
2020-12-27 22:55:39 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+// use context?
+// https://stackoverflow.com/questions/34351804/how-to-declare-a-global-variable-in-react
+let currentTime = new Date();
+let timerDuration = 25;
+// global variables? bruh
+// need to find a way to pass current time and how long the study session will be
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const calculateTimeLeft = (timerDuration) => {
+        let difference = +new Date(currentTime.getTime() + timerDuration * 60000) - +new Date();
+        // let difference = +new Date("12/27/2020, 22:45:00") - +new Date();
+        let timeLeft = {};
+
+        if (difference > 0) {
+            timeLeft = {
+                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+                minutes: Math.floor((difference / 1000 / 60) % 60),
+                seconds: Math.floor((difference / 1000) % 60)
+            };
+        }
+
+        return timeLeft;
+    }
+
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(timerDuration));
+    // const [year] = useState(new Date().getFullYear());
+
+    useEffect( () => {
+    const timer = setTimeout(() => {
+        setTimeLeft(calculateTimeLeft(timerDuration));
+    }, 1000);
+    //clear timeout if the component is unmounted
+    return () => clearTimeout(timer);
+    });
+
+    const timerComponents = [];
+
+    Object.keys(timeLeft).forEach((interval) => {
+        if (!timeLeft[interval]) {
+            return;
+        }
+
+        timerComponents.push(
+            <span>
+                {timeLeft[interval]} {interval}{" "}
+            </span>
+        );
+    });
+
+    return (
+        <div id="timer">
+            {timerComponents.length ? timerComponents : <span>Study session is done! Get some rest :)</span>}
+        </div>
+    );
 }
+
 
 export default App;
