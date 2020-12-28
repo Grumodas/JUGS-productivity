84357dea5ea2308c87f7796c7b86898f898d2106
Kęstutis Grumodas <grumodas.kestutis@gmail.com>
2020-12-28 22:15:27 +0200

    add tabs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import Tabs from "./components/Tabs";
 
 // use context?
 // https://stackoverflow.com/questions/34351804/how-to-declare-a-global-variable-in-react
 let currentTime = new Date();
-let timerDuration = 25;
+let timerDuration = 5;
 // global variables? bruh
 // need to find a way to pass current time and how long the study session will be
 function App() {
-
     const calculateTimeLeft = (timerDuration) => {
         let difference = +new Date(currentTime.getTime() + timerDuration * 60000) - +new Date();
         // let difference = +new Date("12/27/2020, 22:45:00") - +new Date();
@@ -16,10 +16,10 @@ function App() {
 
         if (difference > 0) {
             timeLeft = {
-                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-                minutes: Math.floor((difference / 1000 / 60) % 60),
-                seconds: Math.floor((difference / 1000) % 60)
+                d: Math.floor(difference / (1000 * 60 * 60 * 24)),
+                h: Math.floor((difference / (1000 * 60 * 60)) % 24),
+                min: Math.floor((difference / 1000 / 60) % 60),
+                sec: Math.floor((difference / 1000) % 60)
             };
         }
 
@@ -52,9 +52,24 @@ function App() {
     });
 
     return (
-        <div id="timer">
-            {timerComponents.length ? timerComponents : <span>Study session is done! Get some rest :)</span>}
-        </div>
+        <div>
+            <Tabs> 
+                <div label="Home" id="timer">
+                    {timerComponents.length ? timerComponents : <span>Study session is done! Get some rest :)</span>}
+                </div>
+                <div label="Stats"> 
+                    Statistics about your productics
+                </div> 
+                <div label="About"> 
+                    <p>JU[st]</p> 
+                    <p>G[et]</p> 
+                    <p>S[tarted]</p>
+                </div> 
+                <div label="Settings"> 
+                    Change your settings here!
+                </div> 
+        </Tabs> 
+      </div>
     );
 }
 
